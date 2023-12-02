36b86b33f9cb854909c54e38166e7ad2b1070e6c
Ameet Kaustav <akaustav@users.noreply.github.com>
2023-12-02 16:13:34 -0700

    Show angular version

--- shell/projects/shell/src/app/app.component.ts
+++ shell/projects/shell/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shell';
+  public title = 'shell';
+  public ngVersion = '';
+
+  public constructor() {
+    import('@angular/core').then((ng): void => {
+      this.ngVersion = ng.VERSION.full;
+    });
+  }
 }
