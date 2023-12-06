60d16802d7af3cd855924d2d8b24e445bcecc8fe
Ameet Kaustav <akaustav@users.noreply.github.com>
2023-12-05 22:59:25 -0700

    Add routes for Angular 13, 14, and 15 web components

--- README.md
+++ README.md
@@ -301,7 +301,38 @@
 
     ```typescript
     const routes: Routes = [
-      
+      {
+        path: 'angular13',
+        component: WebComponentWrapper,
+        data: {
+          type: 'module',
+          remoteEntry: 'http://localhost:4201/remoteEntry.js',
+          exposedModule: './Component',
+          elementName: 'angular13-element',
+        } as WebComponentWrapperOptions,
+      },
+
+      {
+        path: 'angular14',
+        component: WebComponentWrapper,
+        data: {
+          type: 'module',
+          remoteEntry: 'http://localhost:4202/remoteEntry.js',
+          exposedModule: './Component',
+          elementName: 'angular14-element',
+        } as WebComponentWrapperOptions,
+      },
+
+      {
+        path: 'angular15',
+        component: WebComponentWrapper,
+        data: {
+          type: 'module',
+          remoteEntry: 'http://localhost:4203/remoteEntry.js',
+          exposedModule: './Component',
+          elementName: 'angular15-element',
+        } as WebComponentWrapperOptions,
+      },
     ];
     ```
 
--- shell/projects/shell/src/app/app-routing.module.ts
+++ shell/projects/shell/src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  WebComponentWrapper,
+  WebComponentWrapperOptions,
+} from '@angular-architects/module-federation-tools';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'angular13',
+    component: WebComponentWrapper,
+    data: {
+      type: 'module',
+      remoteEntry: 'http://localhost:4201/remoteEntry.js',
+      exposedModule: './Component',
+      elementName: 'angular13-element',
+    } as WebComponentWrapperOptions,
+  },
+
+  {
+    path: 'angular14',
+    component: WebComponentWrapper,
+    data: {
+      type: 'module',
+      remoteEntry: 'http://localhost:4202/remoteEntry.js',
+      exposedModule: './Component',
+      elementName: 'angular14-element',
+    } as WebComponentWrapperOptions,
+  },
+
+  {
+    path: 'angular15',
+    component: WebComponentWrapper,
+    data: {
+      type: 'module',
+      remoteEntry: 'http://localhost:4203/remoteEntry.js',
+      exposedModule: './Component',
+      elementName: 'angular15-element',
+    } as WebComponentWrapperOptions,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
